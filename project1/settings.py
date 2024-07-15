5723fbba1eefda5111d20f993b75c551f7697f78
c2winn <sithuwin1892001@gmail.com>
2024-07-15 15:31:45 +0630

    EndOfV2

--- project1/settings.py
+++ project1/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+MEDIA_URL = '/image/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/image')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- store/models.py
+++ store/models.py
@@ -36,29 +36,57 @@ class Product(models.Model):
   shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='products')
   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
   quantity = models.IntegerField(default=0)
-  # image
+  image =models.ImageField(null=True, blank=True)
 
   def __str__(self):
     return self.name
   
-class Cart (models.Model):
+  @property
+  def imageURL(self):
+    try:
+      url = self.image.url
+    except:
+      url = ''
+    return url
+
+  
+class Order (models.Model):
   customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-  # date_ordered = models.DateTimeField(auto_now_add=True)
+  date_ordered = models.DateTimeField(auto_now_add=True)
   transactoinId = models.CharField(max_length=200, null=True)
+  complete = models.BooleanField(default=False, null=True, blank=False)
+
+  @property
+  def get_cart_total(self):
+     orderitems = self.orderitem_set.all()
+     total = sum([item.get_total for item in orderitems])
+     return total
+  
+  @property
+  def get_cart_item(self):
+     orderitems = self.orderitem_set.all()
+     total = sum([item.quantity for item in orderitems])
+     return total
 
   def __str__(self):
     return str(self.id)
 
 
-class CartItem(models.Model):
+class OrderItem(models.Model):
   product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
-  cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, blank=True, null=True)
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
   quantity = models.IntegerField(default=0, null=True, blank=True)
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  @property
+  def get_total(self):
+     total = self.product.price * self.quantity
+     return total
 
 
 class ShippingAddress(models.Model):
   customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-  cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, blank=True, null=True)
+  order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
   address = models.CharField(max_length=200, null=True)
   city = models.CharField(max_length=200, null=True)
   state = models.CharField(max_length=200, null=True)
