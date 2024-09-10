51bc7bc783c9560be30755b44736aba058a95c25
YenXXXW <108684430+YenXXXW@users.noreply.github.com>
2024-09-10 11:52:24 +0630

    add null=True for shop and order

--- store/models.py
+++ store/models.py
@@ -70,7 +70,7 @@ class Product(models.Model):
 class Order (models.Model):
   customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
   date_ordered = models.DateTimeField(auto_now_add=True)
-  shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='orders')
+  shop = models.ForeignKey(Shop,null=True, blank=True, on_delete=models.CASCADE, related_name='orders')
   transactoinId = models.CharField(max_length=200, null=True)
   complete = models.BooleanField(default=False, null=True, blank=False)
 
