46edddc33fb931b2f7048794d0266e485e3629c9
Wai <wai.yan@impact-h.co.jp>
2024-10-10 23:28:09 +0630

    fuck

--- static/js/store.js
+++ static/js/store.js
@@ -1,6 +1,51 @@
 let products = productsData
 selectedShop = 'all'
 document.addEventListener('DOMContentLoaded', function() {
+    
+    // get the products previoulsy filterd with category and shop
+    const searchedShop = sessionStorage.getItem('selectedShop')
+    if(searchedShop) {
+        
+        const form = document.getElementById('category-select-form');
+        const formData = new FormData(form);
+        formData.append('shop_id', searchedShop)
+
+        fetch(form.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-CSRFToken': document.querySelector('[name="csrfmiddlewaretoken"]').value
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response data
+            if (data.products) {
+                products = data.products
+                console.log('here is hte errpr')
+                updateProducts(products)
+                const searchedValue = sessionStorage.getItem('searchedValue')
+                if(searchedValue) {
+                    console.log("here exist the searchVale", searchedValue)
+                    let searchedProducts
+                    if (searchedValue === '')  searchedProducts = products
+                    else{
+                        searchedProducts = products.filter(product => product.name.includes(searchedValue))
+                    }
+                    console.log(searchedProducts)
+                    updateProducts(searchedProducts)
+                }
+            } else {
+                console.error('No products data received.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+
+    // listening for the category change
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
     checkboxes.forEach(checkbox => {
@@ -13,7 +58,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     }
                 });
                 updateDateRequest()
-
             }
         });
     });
@@ -35,7 +79,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updateDateRequest() {
-
         const form = document.getElementById('category-select-form');
         const formData = new FormData(form);
         formData.append('shop_id', selectedShop)
@@ -65,6 +108,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updateProducts(products) {
+        console.log("this is inside the updateProducts", products)
         const productsContainer = document.getElementById('products-container');
         productsContainer.innerHTML = ''; // Clear existing products
         productsContainer.classList.add('col-lg-9')
@@ -80,7 +124,7 @@ document.addEventListener('DOMContentLoaded', function() {
             productElement.innerHTML = `
                 <div class="product">
                     <div class="product-img-container">
-                        <img src="${product.image_url}" alt="${product.name}" class="product-img">
+                        <img src="{% static product.image_url %}" alt="${product.name}" class="product-img">
                     </div>
                     <h3 id="product-price">${product.name}</h3>
                         <p>${product.price} Ks</p>
@@ -103,14 +147,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('shopSelect').addEventListener('change', function() {
         selectedShop = this.value;
+        sessionStorage.setItem('selectedShop', selectedShop)
         updateDateRequest()
     });
 
     const searchProduct = document.getElementById("navSearchInput")
     searchProduct.addEventListener('input', handleSearch)
-    
     function handleSearch(){
-        const searchedProducts = products.filter(product => product.name.includes(searchProduct.value) )
+        console.log(searchProduct.value)
+        sessionStorage.setItem('searchedValue', searchProduct.value)
+        let searchedProducts
+        if(searchProduct.value === ''){
+            searchedProducts = products
+        } else {
+            searchedProducts = products.filter(product => product.name.includes(searchProduct.value))
+        }
+        console.log('suprise')
         updateProducts(searchedProducts)
     }
 
